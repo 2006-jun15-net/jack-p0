b3dc5be063109f45069545dd3c88203fdf32270f
Luis Gomez <luey714@gmail.com>
2020-07-07 12:05:44 -0700

    Added actual input validation to StoreLocation Business Model

--- ProjectZero/StoreApp/BuisnessLogic.Library/Models/StoreLocation.cs
+++ ProjectZero/StoreApp/BuisnessLogic.Library/Models/StoreLocation.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace BuisnessLogic.Library.Models
 {
-    class Class2
+    class StoreLocation
     {
+        private string _locationName;
+
+        public string LocationName
+        {
+            get => _locationName;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("The Store must have a name.", nameof(value));
+                }
+                _locationName = value;
+            }
+        }
     }
 }
